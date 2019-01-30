4c41a42ebe85e6c9557b4e665d6694749c676e6e
Hoang <nghiahoang@kpmg.com.vn>
2019-01-30 11:10:58 +0700

    Initialize code

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    host: process.env.HOST || '127.0.0.1',
+    port: process.env.PORT || 8080
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return `Ping!`;
+    }
+})
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+})
+
+init();
