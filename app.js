d34564d4e3287af0993376399e7dd442ab6c630a
Hoang <nghiahoang@kpmg.com.vn>
2019-02-10 23:14:56 +0700

    refactor

---
+++ app.js
@@ -0,0 +1,23 @@
+const server = require('./server')
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            console.log('resp');
+            return `Pong!`;
+        }
+    });
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log('DEVLOG: ' + err);
+    process.exit(1);
+});
+
+init();
--- server.js
+++ server.js
@@ -2,30 +2,10 @@
 const Hapi = require('hapi');
 
 const server = Hapi.server({
-    // host: process.env.HOST || '0.0.0.0',
-    host: '0.0.0.0',
-    port: process.env.PORT || 80
+    host: process.env.HOST || '0.0.0.0',    
+    port: process.env.PORT || 8000
 });
 
 
-const init = async () => {
 
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
-
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            console.log('resp');
-            return `Pong!`;
-        }
-    });
-}
-
-process.on('unhandledRejection', (err) => {
-    console.log('DEVLOG: ' + err);
-    process.exit(1);
-})
-
-init();
+module.exports = server;
